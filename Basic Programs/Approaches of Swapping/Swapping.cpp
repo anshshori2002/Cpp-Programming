3d3588eadb84b0868f4e15dcf8746c76c45554a9
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2022-11-30 23:47:21 +0530

    Update Swapping.cpp

--- Basic Programs/Approaches of Swapping/Swapping.cpp
+++ Basic Programs/Approaches of Swapping/Swapping.cpp
@@ -9,11 +9,36 @@ int main()
     cin>>a;
     cout<<"b = ";
     cin>>b;
+    
     //Approach-1
     int temp;
     temp = a;
     a = b;
     b = temp;
+    
+    cout<<"The Numbers after swaping :"<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b;
+}
+
+
+
+//Code for Approach 2: This Approach is done without using the temporary Variable.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two Number :"<<endl;
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    
+    //Approach-2
+    a = a + b;
+    b = a - b;
+    a = a - b;
     
     cout<<"The Numbers after swaping :"<<endl;
     cout<<"a = "<<a<<endl;
