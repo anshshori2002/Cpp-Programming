2873eec604c2c8bc991950dcd235b94e95029999
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2022-11-30 23:40:29 +0530

    Update Swapping.cpp

--- Basic Programs/Approaches of Swapping/Swapping.cpp
+++ Basic Programs/Approaches of Swapping/Swapping.cpp
@@ -1,1 +1,25 @@
+//Code for Approach 1:
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two Number :"<<endl;
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    //Approach-1
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    
+    cout<<"The Numbers after swaping :"<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b;
+}
+
+
+
 
