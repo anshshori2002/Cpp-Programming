8f5e828676023412cfeccc87d1359e034de02572
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2022-12-07 17:42:33 +0530

    Create Palindrome.c

---
+++ Basic Programs/Palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,a;
+    printf("Enter the no. :");
+    scanf("%d",&n);
+    a=n;
+    int sum = 0,last = 0;
+    while(n>0)
+    {
+        last = n%10;
+        n = n/10;
+        sum = sum*10 + last;
+    }
+    if(sum==a) printf("%d is a Palindrome",a);
+    else
+    printf("%d is not a Palindrome",a);
+
+    return 0;
+}
