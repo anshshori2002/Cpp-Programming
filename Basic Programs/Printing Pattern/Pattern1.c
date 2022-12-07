4996550078219ac5200b70a7e21c103d4cd5211d
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2022-12-07 18:04:03 +0530

    Update Pattern1.c

--- Basic Programs/Printing Pattern/Pattern1.c
+++ Basic Programs/Printing Pattern/Pattern1.c
@@ -1,1 +1,25 @@
+Pattern1: 1
+          12
+          123
+          1234
+          12345
+  
+Code:
+
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter no. :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
