5ec00a7acd16a16138fd017c45d1b274305ad1c4
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-02-09 20:27:02 +0530

    Update Operations.c

--- Linked List Programs/Doubly Linked List/Operations.c
+++ Linked List Programs/Doubly Linked List/Operations.c
@@ -6,8 +6,38 @@ struct node
     int data;
     struct node* prev;
     struct node* next;
-}head* = NULL;
+}*head = NULL;
+
+void insertion_in_begin(int key)
+{
+    struct node *temp;
+    temp = malloc(sizeof(struct node));
+    if(temp!=NULL)
+    {
+        temp->data = key;
+        temp->prev = NULL;
+        temp->next = head;
+        head = temp;
+    }
+}
+void traversal()
+{
+    struct node *ptr;
+    ptr = head;
+    while(ptr)
+    {
+        printf("->%d",ptr->data);
+        ptr = ptr->next;
+    }
+}
 void main()
 {
-    
+    insertion_in_begin(8);
+    insertion_in_begin(9);
+    insertion_in_begin(23);
+    traversal();
 }
+
+
+// OUTPUT:
+// ->23->9->8
