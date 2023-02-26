ad7ee403f4476dba6642a395a1dd8df82817d5e1
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-02-26 23:42:33 +0530

    Update stack_operations.c

--- Stack and Queue/stack_operations.c
+++ Stack and Queue/stack_operations.c
@@ -1,4 +1,30 @@
-#include<stdio>
+#include<stdio.h>
+#include<limits.h>
+#define size 10
+int Stack[size];
+int top=-1;
+void push(int key)
+{
+    if(top==size-1)
+        printf("Overflow");
+    top++;
+    Stack[top] = key;
+}
+int pop()
+{
+    if(top==-1)
+        return INT_MIN;
+    int temp = Stack[top];
+    top--;
+    return temp;
+}
+void print()
+{
+    for(int i=0; i<top; i++)
+    {
+        printf("%d ",Stack[i]);
+    }
+}
 void main()
 {
   
