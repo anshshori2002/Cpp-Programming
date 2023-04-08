d798d0356151ad29ed323dfef77ccabe04d9243a
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-04-08 23:41:18 +0530

    Update Swapping.c

--- Basic Programs/Approaches of Swapping/Swapping.c
+++ Basic Programs/Approaches of Swapping/Swapping.c
@@ -1,1 +1,21 @@
-
+//Code for Approach 1: This Approach is done by using the Temporary Variable.
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two Number :");
+    printf("a = ");
+    scanf("%d,&a);
+    printf("b = ");
+    scanf("%d",b);
+    
+    //Approach-1
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    
+    printf("The Numbers after swaping :");
+    printf("a = %d",a);
+    printf("b = %d",b);
+}
