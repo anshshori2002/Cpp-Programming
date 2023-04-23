8eeb95a83fc29aef61a832026ff2bdf4c74eff65
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-04-24 00:20:57 +0530

    Update stack_operations.c

--- Stack and Queue/stack_operations.c
+++ Stack and Queue/stack_operations.c
@@ -1,8 +1,12 @@
+//Stack Operations using Array
 #include<stdio.h>
 #include<limits.h>
+#include<stdlib.h>
 #define size 10
 int Stack[size];
 int top=-1;
+
+//Function to push the element in the Stack
 void push(int key)
 {
     if(top==size-1)
@@ -10,7 +14,9 @@ void push(int key)
     top++;
     Stack[top] = key;
 }
-int pop()
+
+//Function to pop the elements from stack
+int pop()   
 {
     if(top==-1)
         return INT_MIN;
@@ -18,16 +24,56 @@ int pop()
     top--;
     return temp;
 }
+
+//Function to print the element in FIFO pattern
 void print()
 {
-    for(int i=0; i<top; i++)
+    for(int i=0; i<=top; i++)
     {
         printf("%d ",Stack[i]);
     }
+    printf("\n");
 }
-void main()
+
+//Function to print the element in reverse
+void reverse_print()
 {
-  push(10);
-  pop();
-  return 0;
+    for(int i=top;i>=0;i--)
+    {
+        printf("%d ",Stack[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    printf("1. Push Element\n");
+    printf("2. Pop Element\n");
+    printf("3. Print Elements\n");
+    printf("4. Print Elements in Reverse Order\n");
+    printf("5. Exit\n");
+    
+    while(1)
+    {
+        printf("Enter the Choice: ");
+        scanf("%d",&n);
+        int a;
+        switch(n)
+        {
+        case 1:printf("Enter the Element: ");
+               scanf("%d",&a);
+               push(a);
+               break;
+        case 2:a = pop();
+               printf("Pop element is : %d\n",a);
+               break;
+        case 3: print();
+                break;
+        case 4: reverse_print();
+                break;
+        default: exit(0);
+        }
+    }
+    
 }
