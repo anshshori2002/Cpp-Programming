6683ddfc7a8a6d6660fd75dfff0d396d25c69805
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-04-26 22:12:01 +0530

    Update Stack_opera_in_linkedlist.c

--- Stack and Queue/Stack_opera_in_linkedlist.c
+++ Stack and Queue/Stack_opera_in_linkedlist.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
+#include<limits.h>
+#define size 10
+struct Stack
+{
+    int arr[size];
+    int top;
+}
+void push(struct Stack *s,int n)
+{
+    if(s->top == size-1)
+        printf("Overflow");
+    s->top++;
+    arr[s->top] = n;
+}
+int pop(struct Stack *s)
+{
+    if(s->top==-1) 
+        return INT_MIN;
+    int temp = arr[s->top];
+    s->top = s->top-1;
+    return temp;
+}
+void print(int top)
+{
+    for(int i=0; i<=top; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
 
+int main()
+{
+    Stack s1,s2;
+    s1.top = -1;
+    s2.top = -1;
+    push(&s1,12);
+    print(s1.top);
+}
