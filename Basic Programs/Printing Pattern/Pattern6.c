867503474ab5b35881b9c71b9ab40d547cebba11
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-07-08 20:15:06 +0530

    Update Pattern6.c

--- Basic Programs/Printing Pattern/Pattern6.c
+++ Basic Programs/Printing Pattern/Pattern6.c
@@ -1,5 +1,23 @@
-#include<bits/stdc++.h>
-void main()
+Pattern6:   1
+            21
+            321
+            4321
+            54321
+
+Code:-
+#include<iostream>
+using namespace std;
+int main()
 {
-  
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int rows=1;rows<=n;rows++)
+    {
+        for(int col=rows;col>=1;col--)
+        {
+            cout<<col;
+        }       
+        printf("\n"); 
+    }
 }
